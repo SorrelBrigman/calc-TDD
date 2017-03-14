1e17c936b4c9b24d61b68a4e6e52283a6858bf85
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-14 10:23:46 -0500

    completed calcuator

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,10 +1,22 @@
 'use strict'
 
-const {assert : {isNumber} } = require('chai');
+const {assert : {isNumber, equal} } = require('chai');
 const calculator = require('../lib/calculator.js');
 
 describe('calculator', ()=>{
   it('should return a number', ()=>{
     isNumber(calculator());
+  });
+  it('should add if it receives + as the second argument', () =>{
+    equal(calculator(1, "+", 2), (1+2))
+  });
+  it('should subtract if it receives - as the second argument', () =>{
+    equal(calculator(1, "-", 2), (1-2))
+  });
+  it('should divide if it receives / as the second argument', ()=>{
+    equal(calculator(1, "/", 2), (1/2))
+  });
+  it('should multiple if it receives * as the second argument', ()=>{
+    equal(calculator(1, "*", 2), (1*2));
   })
 })
--- test/parse-args-test.js
+++ test/parse-args-test.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const {assert: {isNumber, isNotNumber, isArray} } = require('chai');
+const {assert: {isNumber, isNotNumber, isArray, equal} } = require('chai');
 
 const parseArgs = require('../lib/parse-args.js');
 
@@ -8,16 +8,25 @@ describe('parseArgs', ()=>{
   describe('first argument', ()=>{
     it('should return a number', ()=>{
       isNumber(parseArgs.arg1());
+    });
+    it('should equate to the first arguement in the terminal', ()=>{
+      equal(parseArgs.arg1(), process.argv[2]);
     })
   })
   describe('second argument', () =>{
     it('should not return a number', () =>{
       isNotNumber(parseArgs.arg2());
+    });
+    it('should equate to the second arguement in the terminal', ()=>{
+      equal(parseArgs.arg2(), process.argv[3]);
     })
   })
   describe('third argument', ()=>{
     it('should return a number', ()=>{
       isNumber(parseArgs.arg3());
+    })
+    it('should equate to the third arguement in the terminal', ()=>{
+      equal(parseArgs.arg3(), process.argv[4]);
     })
   })
   it('should return the arguments as an array', ()=>{
